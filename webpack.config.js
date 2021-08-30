2437fabbfc41985f85554a6bf3d62a179d1813f4
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-30 13:34:42 +0100

    Make 'npm start' work

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
